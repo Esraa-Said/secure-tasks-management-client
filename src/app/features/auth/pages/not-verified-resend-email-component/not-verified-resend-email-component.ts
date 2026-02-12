8c45b722bc381a1e535c7bb2ee6aff3af8c3732a
Esraa Said <esraa195182@feng.bu.edu.eg>
2026-02-12 17:16:33 +0200

    resend email

--- src/app/features/auth/pages/not-verified-resend-email-component/not-verified-resend-email-component.ts
+++ src/app/features/auth/pages/not-verified-resend-email-component/not-verified-resend-email-component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth-service';
 
 @Component({
   selector: 'app-not-verified-resend-email-component',
-  imports: [RouterLink, FormsModule ],
+  imports: [RouterLink, FormsModule],
   templateUrl: './not-verified-resend-email-component.html',
   styleUrl: './not-verified-resend-email-component.css',
 })
 export class NotVerifiedResendEmailComponent {
+  private authService = inject(AuthService);
+  resMessage: string = '';
+  errMessage: string = '';
 
+  resendVerificationCode(userEmail: string) {
+    this.resMessage = '';
+    this.errMessage = '';
+
+    this.authService.resendVerificationCode(userEmail).subscribe({
+      next: (res) => {
+        this.resMessage = res.message;
+      },
+      error: (err) => {
+        this.errMessage = err.error?.message || 'Internal Server Error';
+      },
+    });
+  }
 }
